c011afbd12f712296af2dc1f3ac1699e85ce90d3
Johanso <johanricardo4@gmail.com>
2024-06-14 09:36:51 -0500

    full page

---
+++ page-full.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Template Name: Full page
+*
+*/
+?>
+
+<?php get_header(); ?>
+  <div class="main-container page full">
+    <?php get_template_part('template-parts/page'); ?>
+  </div>
+<?php get_footer(); ?>
