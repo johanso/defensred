0516405c6d5ce7a4954a415194d65285b86880c3
Johanso <johanricardo4@gmail.com>
2024-06-15 20:50:44 -0500

    page404

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+  <div class="main-container page page-404">
+    <div class="page-404__container" data-text="404">
+      <div class="page-404__title glitch" data-text="404">
+        404
+      </div>
+      <div class="page-404__description glitch" data-text="PAGINA NO ENCONTRADA">
+        PÁGINA NO ENCONTRADA
+      </div>
+    </div>
+  </div>
+<?php get_footer(); ?>
