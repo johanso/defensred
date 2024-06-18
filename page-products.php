249f4c26e41cef26e9d7863d4276965bccb9dd44
Johanso <johanricardo4@gmail.com>
2024-06-18 17:39:59 -0500

    product page

---
+++ page-products.php
@@ -0,0 +1,13 @@
+
+<?php
+/**
+* Template Name: Product Comparison Page
+*
+*/
+?>
+
+<?php get_header(); ?>
+  <div class="main-container page full">
+    hghghhghg
+  </div>
+<?php get_footer(); ?>
