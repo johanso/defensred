43670d68cb0ae8b8406c28af6f13f34526bbee72
Johanso <johanricardo4@gmail.com>
2024-07-14 16:43:37 -0500

    styles fix

--- template-parts/logo.php
+++ template-parts/logo.php
@@ -1,8 +1,5 @@
 <a href="<?php echo site_url('/'); ?>">
   <span class="logo__span">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.svg" alt="logo"> DEFENSRED
-    <span class="flag">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/colombia-icon.png" alt="colombia bandera">
-    </span>
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.svg" alt="logo"> CIBERBLOG
   </span>
 </a>
