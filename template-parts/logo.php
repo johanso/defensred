ff69ea788aff7173252ded3e07ec43d4877b442c
Johanso <johanricardo4@gmail.com>
2024-07-14 17:00:41 -0500

    style logo

--- template-parts/logo.php
+++ template-parts/logo.php
@@ -1,5 +1,6 @@
 <a href="<?php echo site_url('/'); ?>">
   <span class="logo__span">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.svg" alt="logo"> CIBERBLOG
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.svg" alt="logo"> 
+    CIBER<span>BLOG</span>
   </span>
 </a>
