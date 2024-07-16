5200760e178d758b0482ff32798f1dbd1648fb59
Johanso <johanricardo4@gmail.com>
2024-07-15 20:38:45 -0500

    fx styles hero

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,16 +1,11 @@
 <section class="hero">
   <div class="hero__container">
     <div class="hero__content">
-      <h1 class="hero__title">Protección Total, para tu <br /><span>Mundo Digital</span></h1>
-      <p class="hero__text"> Soluciones integrales de ciberseguridad para proteger tu empresa contra amenazas digitales</p>
+      <h1 class="hero__title">Protección Total <span>Para Tu Mundo Digital</span></h1>
+      <p class="hero__text"> Soluciones integrales de ciberseguridad para proteger tu empresa contra amenazas digitales.</p>
       <div class="hero__buttons">
-        <a class="button button--cta button--icon" href="#services">Descubre Nuestras Soluciones</a>
+        <a class="button button--cta-outline button--icon" href="#services">Esto Hacemos</a>
       </div>
     </div>  
   </div>
-
-  <div class="hero__icon">
-    <div class="mouse"></div>
-  </div>
-
 </section>
